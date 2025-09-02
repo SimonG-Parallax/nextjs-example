ee26d5e6681332b4db16a4bc132f79d412609dc8
Simon Garrick <simon.garrick@parall.ax>
2025-09-02 11:41:54 +0100

    remove dupe alt tags

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,8 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image src="/hero-desktop.png" alt="Hero" width={1000} height={760} className="hidden md:block" alt="Screenshots of the dashboard pj desktop version" />
-          <Image src="/hero-mobile.png" alt="Hero" width={560} height={620} className="block md:hidden" alt="Screenshots of the dashboard pj mobile version" />
+          <Image src="/hero-desktop.png" width={1000} height={760} className="hidden md:block" alt="Screenshots of the dashboard pj desktop version" />
+          <Image src="/hero-mobile.png" width={560} height={620} className="block md:hidden" alt="Screenshots of the dashboard pj mobile version" />
         </div>
       </div>
     </main>
